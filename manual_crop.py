e874bc659431ddcd170dd525cd8a52f36467e826
Olivier Friard <olivier.friard@unito.it>
2025-12-11 17:43:56 +0100

    some changes

--- manual_crop.py
+++ manual_crop.py
@@ -186,10 +186,10 @@ def main():
 
     # ffmpeg
     print(
-        f'ffmpeg -i "{sys.argv[1]}" -filter_complex "[0:v]sendcmd=f={str(CROP_OUTPUT)}" "{VIDEO_OUTPUT}" '
+        f'ffmpeg -y -i "{sys.argv[1]}" -filter_complex "[0:v]sendcmd=f={str(CROP_OUTPUT)}" "{VIDEO_OUTPUT}" '
     )
     os.system(
-        f'ffmpeg -i "{sys.argv[1]}" -filter_complex "[0:v]sendcmd=f={str(CROP_OUTPUT)}" "{VIDEO_OUTPUT}" '
+        f'ffmpeg -y -i "{sys.argv[1]}" -filter_complex "[0:v]sendcmd=f={str(CROP_OUTPUT)}" "{VIDEO_OUTPUT}" '
     )
 
 
